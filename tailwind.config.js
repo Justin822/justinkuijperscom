59d3e1ac71be519140ead5727aa41e36efbe1c35
justin822 <justin@justinkuijpers.com>
2023-04-13 21:53:59 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,25 @@ module.exports = {
       fontFamily: {
         mono: ["var(--font-roboto-mono)"],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.700"),
+            h1: {
+              fontFamily: ["var(--font-roboto-mono)"],
+            },
+            h2: {
+              fontFamily: ["var(--font-roboto-mono)"],
+            },
+            h3: {
+              fontFamily: ["var(--font-roboto-mono)"],
+            },
+            h4: {
+              fontFamily: ["var(--font-roboto-mono)"],
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
