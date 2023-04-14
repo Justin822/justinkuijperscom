42f42fa8744b0e9164be012e6ea5e93f2973bdb8
justin822 <justin@justinkuijpers.com>
2023-04-14 21:51:58 +0200

    portfolio and about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,13 @@
+import Sidebar from "@/components/sidebar";
+
 export default function About() {
-  return <div>About page coming soon</div>;
+  return (
+    <div className="flex mx-auto h-screen max-w-4xl mt-36">
+      <Sidebar />
+      <div className=" ">
+        <h1 className="text-3xl font-bold font-mono">About Justin</h1>
+        <p className="max-w-md mt-5">About page coming soon!</p>
+      </div>
+    </div>
+  );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,3 +1,13 @@
+import Sidebar from "@/components/sidebar";
+
 export default function Portfolio() {
-  return <div>Portfolio page coming soon.</div>;
+  return (
+    <div className="flex mx-auto h-screen max-w-4xl mt-36">
+      <Sidebar />
+      <div className=" ">
+        <h1 className="text-3xl font-bold font-mono">Portfolio</h1>
+        <p className="max-w-md mt-5">Portfolio page coming soon!</p>
+      </div>
+    </div>
+  );
 }
